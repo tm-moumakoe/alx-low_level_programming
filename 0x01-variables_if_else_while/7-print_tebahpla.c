0c53a1374de62f7b4624124a83fe3b54f4612ab2
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-02-17 17:52:56 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints z - a, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
