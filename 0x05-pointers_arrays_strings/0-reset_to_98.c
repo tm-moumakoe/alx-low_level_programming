8c54071ee457edf64ad6ba0ad84963f6b6c21ad5
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-02-27 14:38:26 -0800

    0-reset_to_98.c 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98.
+ * @n: pointer to be used in the function.
  */
 
 void reset_to_98(int *n)
