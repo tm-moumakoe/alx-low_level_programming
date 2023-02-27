12006fb7a92fc88675e058d3227cbd1cec1e21ca
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-02-27 14:49:09 -0800

    1-swap.c 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,12 +5,12 @@
  * swap_int - swaps the values of two integers.
  * @a: int to be switched with b.
  * @b: int to be switched with a.
- * @temp: temporary variable to help switch a and b.
  */
 
 void swap_int(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
