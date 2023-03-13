7cce30883bdec32d0873615c76d728229209225e
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-03-13 15:59:15 -0700

    3-mul.c 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,12 @@
 
 int _atoi(char *s)
 {
-        int i = 0, d = 0, n = 0, length = 0, f = 0, digit = 0;
-
-        while (s[length] != '\0')
-                length++;
-        while (i < length && f == 0)
+        int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
+	
+	while (s[len] != '\0')
+		len ++;
+	
+	while (i < len && f == 0)
         {
                 if (s[i] == '-')
                         ++d;
